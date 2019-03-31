b00bf61be51a83f0e4ebfc120076cf5aea154bce
hpr <dev.ac.hpr@gmail.com>
2019-03-31 20:32:53 +0530

    bas final hi hai

---
+++ coc.sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 31, 2019 at 05:01 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `code`
+--
+
+CREATE TABLE `code` (
+  `code_id` int(10) NOT NULL,
+  `file_type` text NOT NULL,
+  `submit_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game`
+--
+
+CREATE TABLE `game` (
+  `game_id` int(50) NOT NULL,
+  `player1_id` int(10) NOT NULL,
+  `player2_id` int(10) NOT NULL,
+  `winner_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gameboard`
+--
+
+CREATE TABLE `gameboard` (
+  `id` int(11) NOT NULL,
+  `user` varchar(40) NOT NULL,
+  `with` varchar(80) NOT NULL,
+  `winner` varchar(40) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `VALID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_board`
+--
+
+CREATE TABLE `game_board` (
+  `index` int(10) NOT NULL,
+  `game_id` int(10) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `valid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `game_board`
+--
+DELIMITER $$
+CREATE TRIGGER `valid` AFTER UPDATE ON `game_board` FOR EACH ROW begin
+Update game_board set valid=1;
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE `player` (
+  `player_id` int(10) NOT NULL,
+  `code_id` int(10) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `roll` varchar(40) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`id`, `email`, `name`, `roll`, `time`) VALUES
+(37, 'a@1.2', 'a', 'NITD', '0000-00-00 00:00:00'),
+(38, 'b@1.2', 'b', 'NITD', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registered`
+--
+
+CREATE TABLE `registered` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `roll` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `pass` varchar(300) NOT NULL,
+  `activation` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `registered`
+--
+
+INSERT INTO `registered` (`id`, `name`, `roll`, `email`, `pass`, `activation`) VALUES
+(49, 'a', 'NITD', 'a@1.2', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb', ''),
+(50, 'b', 'NITD', 'b@1.2', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `institute` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `code`
+--
+ALTER TABLE `code`
+  ADD PRIMARY KEY (`code_id`);
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`game_id`);
+
+--
+-- Indexes for table `gameboard`
+--
+ALTER TABLE `gameboard`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `game_board`
+--
+ALTER TABLE `game_board`
+  ADD PRIMARY KEY (`index`),
+  ADD KEY `game_id` (`game_id`);
+
+--
+-- Indexes for table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`player_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `code_id` (`code_id`,`email`);
+
+--
+-- Indexes for table `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registered`
+--
+ALTER TABLE `registered`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gameboard`
+--
+ALTER TABLE `gameboard`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `game_board`
+--
+ALTER TABLE `game_board`
+  MODIFY `index` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `players`
+--
+ALTER TABLE `players`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `registered`
+--
+ALTER TABLE `registered`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `game_board`
+--
+ALTER TABLE `game_board`
+  ADD CONSTRAINT `game_board_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`game_id`);
+
+--
+-- Constraints for table `player`
+--
+ALTER TABLE `player`
+  ADD CONSTRAINT `player_ibfk_1` FOREIGN KEY (`code_id`) REFERENCES `code` (`code_id`),
+  ADD CONSTRAINT `player_ibfk_2` FOREIGN KEY (`email`) REFERENCES `user` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
